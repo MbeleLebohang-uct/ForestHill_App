4e43f5372f2f07c94e43211a9b43a9bb03ce5724
Mbele Lebohang <micheallm95@gmail.com>
2017-11-26 21:09:08 +0200

    Added all basic static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
-  get  'static_pages/about'
+
+  get '/about', to:'static_pages#about'
+  get '/blog', to:'static_pages#blog'
+  get '/culture', to:'static_pages#culture'
+  get '/facilities', to:'static_pages#facilities'
+  get '/gallery', to:'static_pages#gallery'
+
 
 end
